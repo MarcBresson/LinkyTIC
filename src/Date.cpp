738a27b014336e686b02ca1091aac32f6006e78c
Marc Bresson <marco.bresson@gmail.com>
2023-01-05 14:18:13 +0100

    doc & refactor: added var doc and correct type.
    To avoid messy int sizes, the size is now predetermined using stdint.
    The doc for each variable is now provided (in french).

--- src/Date.cpp
+++ src/Date.cpp
@@ -1,10 +1,11 @@
 #include "Date.h"
-#include "Arduino.h"
 
-char* substring(const char* _buffer_value, const int start){
+
+char* substring(const char* _buffer_value, const uint8_t start){
     char str[2];
-    int i_buffer = start;
-    for(int i_str = 0; i_str < 2; i_str++){
+    uint8_t i_buffer = start;
+    
+    for(uint8_t i_str = 0; i_str < 2; i_str++){
         str[i_str] = _buffer_value[i_buffer + i_str];
         i_str++;
     }
@@ -17,6 +18,7 @@ void Date::parse(const char* horodate){
     annee = atoi(substring(horodate, 1));
     mois = atoi(substring(horodate, 3));
     jours = atoi(substring(horodate, 5));
+
     heures = atoi(substring(horodate, 7));
     minutes = atoi(substring(horodate, 9));
     secondes = atoi(substring(horodate, 11));
--- src/Date.h
+++ src/Date.h
@@ -1,16 +1,23 @@
 #ifndef _LINKYDATE
 #define _LINKYDATE
 
+#include <stdint.h>
+
+
 class Date {
     public:
-        enum MODE {NORMAL, DEGRADE};
-        char saison;
-        uint8_t annee;
-        uint8_t mois;
-        uint8_t jours;
-        uint8_t heures;
-        uint8_t minutes;
-        uint8_t secondes;
+        enum MODE {NORMAL, DEGRADE};    //Dans le cas où une horodate a été obtenue avec une horloge temps réel en mode dégradé (cas de fonctionnement du compteur qui a débordé son autonomie de marche de l’horloge et tant que son horloge n’a pas pu être resynchronisée par programmation), alors l'information de saison est codée avec une lettre minuscule. [page 17, notice Enedis-NOI-CPT_54E]
+        MODE mode;
+        
+        char saison;        // P, E, A, H (printemps, été, automne, hiver). Représente la saison en cours.
+
+        uint8_t annee;      // L'année sur 2 caractères (e.g. 18 pour 2018)
+        uint8_t mois;       // Le mois (1-12)
+        uint8_t jours;      // Le jour dans le mois (1-31)
+
+        uint8_t heures;     // L'heure dans le jour (0-23)
+        uint8_t minutes;    // Les minutes dans l'heure (0-59)
+        uint8_t secondes;   // Les secondes dans les minutes (0-59)
 
         void parse(const char* horodate);
 };
